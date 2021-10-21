e1aa1d570d5be4565465ccf1295b3080007b5dda
Yonatan Vigilio <80126199+VigilioYonatan@users.noreply.github.com>
2021-10-21 09:41:40 -0500

    primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,16 @@
+import '../css/componentes.css';
+
+
+
+export const saludar = ( nombre ) => {
+    sdscdccs
+    sdadc
+
+    console.log('Creando etiqueta h1, en el HTML!');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${ nombre }`;
+
+    document.body.append( h1 );
+
+}
